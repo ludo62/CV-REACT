d560106dea14dc7ae56a9ca71c37179bd776bf6e
Ludo62230 <90885543+ludo62@users.noreply.github.com>
2021-12-02 22:11:52 +0100

    update navigation left

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import profil from '../media/photo.jpg';
 
 const Navigation = () => {
@@ -8,6 +9,51 @@ const Navigation = () => {
                 <div className="idContent">
                  <img src = {profil} alt="profil cv" />
                    <h3>Fournier Ludovic</h3>
+                </div>
+            </div>
+
+            <div className="navigation">
+                <ul>
+                    <li>
+                    <NavLink exact to="/" activeClassName="active">
+                        <i className="fas fa-home"></i>
+                        <span>Accueil</span>
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/competences" activeClassName="active">
+                        <i className="fas fa-mountain"></i>
+                        <span>Compétences</span>
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/portfolio" activeClassName="active">
+                        <i className="fas fa-images"></i>
+                        <span>Portfolio</span>
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink exact to="/contact" activeClassName="active">
+                        <i className="fas fa-address-book"></i>
+                        <span>Contact</span>
+                    </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="socialNetwork">
+                <ul>
+                    <li>
+                       <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-linkedin"></i></a> 
+                    </li>
+                    <li>
+                       <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+                            <i className="fab fa-github"></i></a> 
+                    </li>
+                </ul>
+                <div className="signature">
+                    <p>Fournier - 2021</p>
                 </div>
             </div>
         </div>
